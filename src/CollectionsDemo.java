a0b4db84dfc256b7eb9d654e951329c5b79f584b
Peter Broadbent <103116958+PetyrB2@users.noreply.github.com>
2022-11-18 14:13:10 +0000

    Using Iterable

--- src/CollectionsDemo.java
+++ src/CollectionsDemo.java
@@ -1,16 +1,22 @@
-
-import java.GenericList;
-
 public class CollectionsDemo {
 
-	
-	
 	public static void main(String[] args) {
+		// Create a GenericList called list
 		var list = new GenericList<String>();
+		
+		var iterator = list.iterator();
+		
+		while(iterator.hasNext()) {
+			var current = iterator.next();
+			System.out.println(current);
+		}
+		
 			list.add("Alpha");
 			list.add("Beta");
 			
-			for (var item : list.items)
+			for (var item : list)
 				System.out.println(item);
-	}
+
+	
+	}//end main
 }
--- src/GenericList.java
+++ src/GenericList.java
@@ -1,17 +1,50 @@
+import java.util.Iterator;
 
-public class GenericList<T> {
+public class GenericList<T> implements Iterable<T> {
 
 	public T[] items = (T[]) new Object[10];
+	
 	private int count;
-	
-	
+
 	public void add(T item) {
 		items[count++] = item;
 	}// end of add()
-	
-	
+
 	public T get(int index) {
 		return items[index];
 	}// end of get()
-}//EOF
 
+	@Override
+	public Iterator<T> iterator() {
+		return new ListIterator(this);
+	}//end of Iterator<T>
+	
+
+
+
+public class ListIterator implements Iterator<T> {
+
+	private GenericList<T> list;
+	private int index;
+	
+	public ListIterator(GenericList<T> list) {
+				this.list = list;
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return (index < list.count);
+	}
+
+	@Override
+	public T next() {
+		return list.items[index++];
+	}
+	
+}
+	
+		
+
+}//end class 
+	
