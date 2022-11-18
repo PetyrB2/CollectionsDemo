c5526a114999c1b5f262069ab8e75bc7be0fa59e
Peter Broadbent <103116958+PetyrB2@users.noreply.github.com>
2022-11-18 18:05:53 +0000

    Tidied

--- src/Collections/CollectionsDemo.java
+++ src/Collections/CollectionsDemo.java
@@ -33,27 +33,70 @@ public class CollectionsDemo {
 		System.out.println("Collection after adding using other method");
 		System.out.println("");
 		System.out.println(collection);
-
+		System.out.println("Removal");
+		System.out.println("-------");
+		System.out.println("");
 		System.out.println("Removing 'so' from collection...");
 		System.out.println("");
 		collection.remove("so");
 		System.out.println("New collection");
 		System.out.println("--------------");
+		System.out.println("");
 		System.out.println(collection);
 		System.out.println("");
 
 		// Does collection Container a Char or String
 		System.out.println("Does collection contain an 'A' ?");
 		var containsA = collection.contains("A");
-		System.out.println("\n\t" + containsA);
+		System.out.println("\t" + containsA);
 		System.out.println("");
+		System.out.println("Questions:");
+		System.out.println("----------");
 		System.out.println("Does collection contain an 'm' ?");
 		var contains_m = collection.contains("m");
-		System.out.println("\n\t" + contains_m);
-
+		System.out.println("\t" + contains_m);
+		System.out.println("");
 		System.out.println("Not sure why this breaks -- should be True");
+		System.out.println("");
 
-		// Emptying colleciton is the clear() method
+		// NEW SOLUTION
+		System.out.println("(A Solution using boolean)");
+		System.out.println("");
+		// create a boolean named result with our question
+		boolean resultAlpha = collection.contains("Alpha");
+		// print out the answer
+		System.out.println("Does the colleciton contain 'Alpha'?");
+		System.out.println("\t" + resultAlpha);
+		System.out.println("");
+		// create a boolean named result with our question
+		boolean resulta = collection.contains("Alpha");
+		// print out the answer
+		System.out.println("Does the colleciton contain an 'a'?");
+		System.out.println("\t" + resulta);
+		System.out.println("");
+
+		System.out.println("------------------------------------");
+		System.out.println("Collection After Removal: ");
+		System.out.println("-------------------------");
+		System.out.println("Making a new collection named 'other'...");
+		System.out.println("");
+
+		// create new collection
+		Collection<String> other = new ArrayList<>();
+		// copy collection to other
+		System.out.println("Copying collection data to other using other.addAll(collection)");
+		other.addAll(collection);
+
+		// Equality checks
+		System.out.println("");
+		System.out.println("Are the two collections '=='");
+		System.out.println("\n\t" + (collection == other));
+		System.out.println("");
+		System.out.println("Are the two collections 'equal'");
+		System.out.println("\n\t" + collection.equals(other));
+		System.out.println("------------------------");
+
+		// Emptying collection using the clear() method
 		System.out.println("");
 		System.out.println("Emptying collection....");
 		collection.clear();
@@ -61,15 +104,7 @@ public class CollectionsDemo {
 		System.out.println("Is collection empty?");
 		System.out.println("\t" + collection.isEmpty());
 		System.out.println("");
-
-		Collection<String> other = new ArrayList<>();
-		other.addAll(collection);
-
-		System.out.println("Are the two collections '=='");
-		System.out.println("\n\t" + (collection == other));
-		System.out.println("");
-		System.out.println("Are the two collections 'equal'");
-		System.out.println("\n\t" + collection.equals(other));
+		System.out.println("--------EOF---------");
 
 	}// end show()
 
