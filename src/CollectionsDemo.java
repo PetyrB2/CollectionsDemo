612874590c0641b976af9dfcf79af9f2f2d50478
Peter Broadbent <103116958+PetyrB2@users.noreply.github.com>
2022-11-18 14:21:55 +0000

    Tidying

--- src/CollectionsDemo.java
+++ src/CollectionsDemo.java
@@ -1,6 +1,22 @@
 public class CollectionsDemo {
 
 	public static void main(String[] args) {
+		// Intro
+		System.out.println("========================");
+		System.out.println("Title: Iterable");
+		System.out.println("Genre: Java Intermediate");
+		System.out.println("Author: Peter Broadbent");
+		System.out.println("Language: Java \n" + "Version: " + System.getProperty("java.version"));
+		System.out.println("Description:\n\tIterable is an object which can be looped over or "
+				+ "\n\titerated over with the help of a for loop."
+				+ "\n\tWe create a GenericList class file and implement "
+				+ "\n\tthe Iterable interface to for loop through the data. "
+				+ "\n\tWe can loop through any List Type using Generics."
+				+ "\n\t");
+		System.out.println("===================================================================\n");
+		System.out.println("");
+
+		
 		// Create a GenericList called list
 		var list = new GenericList<String>();
 		
